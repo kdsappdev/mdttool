a7006dde4067218078d76bb2ee7118df6cb3532c
kds.app.dev <kds.app.dev@c4788cab-72bc-0776-2b3f-0885dcd7b80f>
2013-11-30 09:31:55 +0000

    增加设置表，及列备注信息插件

---
+++ MDT_Tools/MDT.Tools.DB.SetTableAndTableColumnComment.Plugin/SetCommentPlugin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDT.Tools.DB.SetTableAndTableColumnComment.Plugin
+{
+    public class SetCommentPlugin
+    {
+    }
+}
