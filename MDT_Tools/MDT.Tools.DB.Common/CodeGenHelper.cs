930998cef66eec9158257980f14a897930928222
kds.app.dev <kds.app.dev@gmail.com>
2013-12-04 10:15:36 +0000

    DB公共资源

---
+++ MDT_Tools/MDT.Tools.DB.Common/CodeGenHelper.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+namespace MDT.Tools.DB.Common
+{
+    public sealed class CodeGenHelper
+    {
+        private CodeGenHelper()
+        { }
+        public static string GetDbProviderString(string dbType)
+        {
+            string str = "";
+            switch (dbType)
+            {
+                case "Oracle":
+                    str = "OracleHelper";
+                    break;
+                case "Sql Server":
+                    str = "SqlHelper";
+                    break;
+            }
+            return str;
+        }
+
+     
+
+        public static string StrFirstToUpperRemoveUnderline(string str)
+        {
+            string temp = str;
+            if (!string.IsNullOrEmpty(str))
+            {
+                temp = temp.Trim(new[] { '#', ' ', ';', '\r', '\n' });
+                string[] temps = temp.ToLower().Split(new char[] { '_' });
+                temp = "";
+                foreach (string s in temps)
+                {
+                    temp += StrFirstToUpper(s);
+                }
+            }
+            return temp;
+        }
+        public static string StrFirstToLowerRemoveUnderline(string str)
+        {
+            string temp = str;
+            if (!string.IsNullOrEmpty(str))
+            {
+                temp =  temp.Trim(new[] { '#', ' ', ';', '\r', '\n' });
+                string[] temps = temp.ToLower().Split(new char[] { '_' });
+               
+                if (temps.Length > 0)
+                {
+                    temp = "";
+                    temp += StrFirstToLower(temps[0]);
+                    for (int i = 1; i < temps.Length; i++)
+                    {
+                        temp += StrFirstToUpper(temps[i]);
+                    }
+                }
+                
+            }
+            return temp;
+        }
+        public static string StrFieldWith_(string str)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {
+               
+                str ="_"+ StrFirstToLowerRemoveUnderline(str);
+            }
+            return str;
+        }
+        public static string StrProperty(string str)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {
+                str = StrFirstToUpperRemoveUnderline(str);
+            }
+            return str;
+        }
+        public static string StrFirstToUpper(string str)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {               
+                str = str[0].ToString().ToUpper() + str.Substring(1, str.Length - 1);
+            }
+            return str;
+        }
+        public static string StrFirstToLower(string str)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {
+                str = str[0].ToString().ToLower() + str.Substring(1, str.Length - 1);
+            }
+            return str;
+        }
+        public static string GetDefaultValueByDataType(string str, string defaultValue)
+        {
+            string temp = "\"\"";
+            if (!string.IsNullOrEmpty(str))
+            {
+                if (!string.IsNullOrEmpty(defaultValue))
+                defaultValue=defaultValue.Trim(new char[]{'\'',' ','\r','\n','\t'});
+           
+                switch (str)
+                {
+                    case "string":
+                        temp = !string.IsNullOrEmpty(defaultValue) && !defaultValue.Equals("EMPTY_CLOB()")
+                                   ? "\"" + defaultValue + "\""
+                                   : "\"\"";
+                        break;
+                    case "decimal":
+                    case "decimal?":
+                        temp = !string.IsNullOrEmpty(defaultValue) ? defaultValue + "M" : "0M";
+                        break;
+                    case "int?":
+                    case "int":
+                        temp = !string.IsNullOrEmpty(defaultValue) ? defaultValue : "0";
+                        break;
+                    case "long":
+                    case "long?":
+                        temp = !string.IsNullOrEmpty(defaultValue) ? defaultValue : "0";
+                        break;
+                    case "DateTime?":
+                    case "DateTime":
+                        temp = "DateTime.Now";                        
+                        break;
+                    default:
+                        temp = "\"\"";
+                        break;
+                }
+            }
+            return temp;
+        }
+    }
+
+}
---
+++ MDT_Tools/MDT.Tools.DB.Common/DBSubPlugin.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using MDT.Tools.Core.Plugin;
+
+namespace MDT.Tools.DB.Common
+{
+    public abstract class DBSubPlugin : AbstractPlugin
+    {
+
+        #region load,unload
+        protected override void load()
+        {
+            subscribe(PluginShareHelper.DBPlugin_BroadCast_CheckTableNumberIsGreaterThan0, this);
+            AddContextMenu();
+        }
+        public override void BeforeTerminating()
+        {
+            unsubscribe(PluginShareHelper.DBPlugin_BroadCast_CheckTableNumberIsGreaterThan0, this);
+            Application.MainContextMenu.Items.Remove(_tsiGen);
+        }
+        #endregion
+
+        #region onNotify
+        public override void onNotify(string name, object o)
+        {
+            base.onNotify(name, o);
+            if (PluginShareHelper.DBPlugin_BroadCast_CheckTableNumberIsGreaterThan0.Equals(name) && o.GetType().IsValueType)
+            {
+                var flag = (bool)o;
+                SetEnable(flag);
+            }
+        }
+        protected delegate void SimpleBool(bool flag);
+        protected void SetEnable(bool flag)
+        {
+            if (Application.MainContextMenu.InvokeRequired)
+            {
+                var s = new SimpleBool(SetEnable);
+                Application.MainContextMenu.Invoke(s, new object[] { flag });
+            }
+            else
+            {
+                _tsiGen.Enabled = flag;
+            }
+        }
+        #endregion
+
+       
+
+        #region 增加上下文菜单
+        protected readonly ToolStripMenuItem _tsiGen = new ToolStripMenuItem();
+
+        protected delegate void Simple();
+        protected virtual void AddContextMenu()
+        {
+            if (Application.MainContextMenu.InvokeRequired)
+            {
+                var s = new Simple(AddContextMenu);
+                Application.MainContextMenu.Invoke(s, null);
+            }
+            else
+            {
+                _tsiGen.Enabled = false;
+                _tsiGen.DisplayStyle = ToolStripItemDisplayStyle.ImageAndText;
+                Application.MainContextMenu.Items.Add(_tsiGen);
+            }
+        }
+        #endregion
+
+        protected virtual void process(DataRow[] drTable, AbstractHandler handler)
+        {
+             
+            var dbName = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_DBCurrentDBName) as string;
+            var dbType = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_DBCurrentDBType) as string;
+            var dsTableColumn = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_DBCurrentDBAllTablesColumns) as DataSet;
+            var dsTablePrimaryKey = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_DBCurrentDBTablesPrimaryKeys) as DataSet;
+
+            var dBtable = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_DBtable) as string;
+            var dBtablesColumns = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_DBtablesColumns) as string;
+
+            var dBviews = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_DBviews) as string;
+            var dBtablesPrimaryKeys = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_DBtablesPrimaryKeys) as string;
+            var tsslMessage = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_tsslMessage) as ToolStripStatusLabel;
+            var tspbLoadDBProgress = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_tspbLoadDBProgress) as ToolStripProgressBar;
+            var originalEncoding = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_OriginalEncoding) as string;
+            var targetEncoding = getObject(PluginShareHelper.DBPluginKey, PluginShareHelper.DBPlugin_TargetEncoding) as string;
+
+            handler.tsiGen = _tsiGen;
+            handler.DBtable = dBtable;
+            handler.DBtablesColumns = dBtablesColumns;
+            handler.DBviews = dBviews;
+            handler.DBtablesPrimaryKeys = dBtablesPrimaryKeys;
+            handler.dbName = dbName;
+            handler.dbType = dbType;
+            handler.tsslMessage = tsslMessage;
+            handler.tspbLoadDBProgress = tspbLoadDBProgress;
+            handler.MainContextMenu = Application.MainContextMenu;
+            handler.Panel = Application.Panel;
+            handler.dsTableColumn = dsTableColumn;
+            handler.dsTablePrimaryKey = dsTablePrimaryKey;
+            handler.PluginName = PluginName + "(V" + Version + ")";
+            if (!string.IsNullOrEmpty(originalEncoding) && !string.IsNullOrEmpty(targetEncoding))
+            {
+                handler.OriginalEncoding = Encoding.GetEncoding(originalEncoding);
+                handler.TargetEncoding = Encoding.GetEncoding(targetEncoding);
+            }
+          
+
+            handler.process(drTable, dsTableColumn, dsTablePrimaryKey);
+        }
+    }
+}
