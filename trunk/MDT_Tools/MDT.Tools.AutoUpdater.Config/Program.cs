49e1bbe679110c0dd1d78a256cb9bc8fef7eaae4
kds.app.dev@gmail.com <kds.app.dev@gmail.com@c4788cab-72bc-0776-2b3f-0885dcd7b80f>
2013-12-25 05:07:30 +0000

    增加 autoUpdater 升级文件制作

---
+++ trunk/MDT_Tools/MDT.Tools.AutoUpdater.Config/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace MDT.Tools.AutoUpdater.Config
+{
+    class Program
+    {
+
+        //获取当前目录
+        //static string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        static string currentDirectory = System.Environment.CurrentDirectory;
+        //服务端xml文件名称
+        static string serverXmlName = "AutoUpdateService.xml";
+        //更新文件URL前
+        static string url = System.Configuration.ConfigurationSettings.AppSettings["url"];
+
+        static void Main(string[] args)
+        {
+            //创建文档对象
+            XmlDocument doc = new XmlDocument();
+            //创建根节点
+            XmlElement root = doc.CreateElement("updateFiles");
+            //头声明
+            XmlDeclaration xmldecl = doc.CreateXmlDeclaration("1.0", "utf-8", null);
+            doc.AppendChild(xmldecl);
+            //获取当前目录对象
+            DirectoryInfo dicInfo = new DirectoryInfo(currentDirectory);
+            //调用递归方法组装xml文件
+            PopuAllDirectory(doc, root, dicInfo);
+            //追加节点
+            doc.AppendChild(root);
+            //保存文档
+            doc.Save(serverXmlName);
+        }
+
+        //递归组装xml文件方法
+        private static void PopuAllDirectory(XmlDocument doc, XmlElement root, DirectoryInfo dicInfo)
+        {
+            foreach (FileInfo f in dicInfo.GetFiles())
+            {
+                //排除当前目录中生成xml文件的工具文件
+                List<string> lt = new List<string>();
+                lt.Add("ConsoleApplication1.exe");
+                lt.Add("AutoupdateService.xml");
+                lt.Add("AutoUpdater.config");
+                lt.Add("MDT.Tools.AutoUpdater.exe");
+                lt.Add("MDT.Tools.AutoUpdater.exe.config");
+                lt.Add("MDT.Tools.AutoUpdater.Config.exe");
+                lt.Add("MDT.Tools.AutoUpdater.Config.exe.config");
+                if (!lt.Contains(f.Name) && !f.Name.EndsWith("pdb"))
+                {
+                    string path = dicInfo.FullName.Replace(currentDirectory, "").Replace("\\", "/");
+                    string str = url + path + "/" + f.Name;
+                    if (!(str.IndexOf("svn") > 0))
+                    {
+                        string version = FileVersionInfo.GetVersionInfo(f.FullName).FileVersion;
+                        XmlElement child = doc.CreateElement("file");
+                        child.SetAttribute("path", f.Name);
+                        child.SetAttribute("url", str);
+                        child.SetAttribute("lastver", string.IsNullOrEmpty(version) ? "1.0.0.0" : version);
+                        child.SetAttribute("size", f.Length.ToString());
+                        child.SetAttribute("needRestart", "true");
+                        root.AppendChild(child);
+                    }
+                }
+            }
+
+            foreach (DirectoryInfo di in dicInfo.GetDirectories())
+                PopuAllDirectory(doc, root, di);
+
+
+        }
+    }
+}
