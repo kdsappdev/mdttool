95377c089e0f8475c4779ad48f6733d22e8b4559
kds.app.dev <kds.app.dev@c4788cab-72bc-0776-2b3f-0885dcd7b80f>
2014-01-18 08:19:48 +0000

    增加 lua插件引擎，可以使用lua脚本来编写插件

---
+++ trunk/MDT_Tools/MDT.Tools.Lua.Plugin/Utils/FilePathHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using MDT.Tools.Core.Utils;
+
+namespace MDT.Tools.Lua.Plugin.Utils
+{
+    internal class FilePathHelper
+    {
+        private FilePathHelper()
+        { }
+       
+        public static readonly string LuaScriptPath = Application.StartupPath + "\\script\\";
+    }
+}
