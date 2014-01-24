bc747ac554a16d2d66f01a5679657faf0582bcdc
kds.app.dev@gmail.com <kds.app.dev@gmail.com@c4788cab-72bc-0776-2b3f-0885dcd7b80f>
2014-01-24 09:31:38 +0000

    增加log4net

---
+++ trunk/MDT_Tools/MDT.Tools.Core/Log/Log4netLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MDT.Tools.Core.Utils;
+
+namespace MDT.Tools.Core.Log
+{
+    public class Log4netLog : ILog
+    {
+        private Type _currentType = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType;
+        private log4net.ILog _log = null;
+        public Log4netLog()
+        {
+            _log = log4net.LogManager.GetLogger(_currentType);
+            log4net.Config.XmlConfigurator.Configure(
+                 new FileInfo(@"control/log4netConfig.config"));
+        }
+
+        public void Debug(string str)
+        {
+            _log.Debug(str);
+        }
+
+        public void Warn(string str)
+        {
+            _log.Warn(str);
+        }
+
+        public void Error(Exception ex)
+        {
+            _log.Error(ex);
+        }
+    }
+}
